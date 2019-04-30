7b1151264ff0567a17e61c1745cc20d1134dbdaa
Chris <chriskatins@gmail.com>
2019-05-01 00:57:18 +0200

    Now plotting trajectory with matplotlib

---
+++ mav_sim/draw.py
@@ -0,0 +1,44 @@
+import matplotlib as mpl
+from mpl_toolkits.mplot3d import Axes3D
+from numpy import *
+import matplotlib.pyplot as plot
+
+def draw_traj(waypoint0, waypoint1, trajectory):
+    mpl.rcParams['legend.fontsize'] = 10
+
+    t = linspace(0,5,1000)
+    x_path = trajectory[0] * t ** 4 + trajectory[1] * t ** 3 + trajectory[2] * t ** 2 + trajectory[3] * t + trajectory[4]
+    y_path = trajectory[5] * t ** 4 + trajectory[6] * t ** 3 + trajectory[7] * t ** 2 + trajectory[8] * t + trajectory[9]
+    z_path = trajectory[10] * t ** 4 + trajectory[11] * t ** 3 + trajectory[12] * t ** 2 + trajectory[13] * t + trajectory[14]
+    phi_path = trajectory[15] * t ** 2 + trajectory[16] * t + trajectory[17]
+
+    # add 3D plot
+    ax = plot.subplot2grid((4,7),(0,0), colspan = 3, rowspan = 3,projection='3d') #create Axes3D object, which can plot in 3D
+    ax.plot(x_path, y_path, z_path, label='3D Trajectory') # plot trajectory
+    ax.plot([waypoint0.x],[waypoint0.y],[waypoint0.z],'ro') # plot start
+    ax.plot([waypoint1.x], [waypoint1.y], [waypoint1.z], 'ro') # plot end
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    ax.legend()
+
+    # add 2D plot of X over time
+    ax = plot.subplot2grid((4,7),(0,4), colspan = 3)
+    ax.plot(t, x_path)
+    ax.set_ylabel('X')
+    # add 2D plot of Y over time
+    ax = plot.subplot2grid((4,7),(1,4), colspan = 3)
+    ax.plot(t, y_path)
+    ax.set_ylabel('Y')
+    # add 2D plot of Z over time
+    ax = plot.subplot2grid((4,7),(2,4), colspan = 3)
+    ax.plot(t, z_path)
+    ax.set_ylabel('Z')
+    # add 2D plot of Phi over time
+    ax = plot.subplot2grid((4,7),(3,4), colspan = 3)
+    ax.plot(t, phi_path)
+    ax.set_ylabel('Phi')
+    ax.set_xlabel('Time')
+
+    # print to screen
+    plot.show()
--- mav_sim/trajectory_planner.py
+++ mav_sim/trajectory_planner.py
@@ -1,6 +1,7 @@
 from numpy import *
 from scipy.sparse import csc_matrix
 from qpsolvers import solve_qp
+import draw
 
 class Waypoint:
     def __init__(self, x, y ,z, phi, time):
@@ -102,4 +103,6 @@ def planner(state, traj_x, traj_y, traj_z, traj_phi):
     b[7, 0] = waypoint1.phi
     b = hstack(b)  # convert to hstack for performance
 
-    print("QP solution:", solve_qp(P, q, G, h, A, b, solver="osqp"))  # solver = "quadprog" (default), "cvxpy", "osqp"
+    trajectory = solve_qp(P, q, G, h, A, b, solver="osqp") # solver = "quadprog" (default), "cvxpy", "osqp"
+    print("QP solution:", trajectory )
+    draw.draw_traj(waypoint0, waypoint1, trajectory)
