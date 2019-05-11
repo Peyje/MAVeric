0b94e4fa99fc86e1dbbae151ab87d656e7213d53
Chris <chriskatins@gmail.com>
2019-05-11 15:14:56 +0200

    Non-joint optimization (kind of) working

--- traj_var.py
+++ traj_var.py
@@ -17,8 +17,11 @@ class Waypoint:
 if __name__ == "__main__":
     waypoints = []
     waypoints.append(Waypoint(0, 0, 2, 0, 0))
-    waypoints.append(Waypoint(5, 5, 10, 3, 5))
-    waypoints.append(Waypoint(-2, 5, 3, 1, 10))
+    waypoints.append(Waypoint(5, 5, 10, 3, 6))
+    waypoints.append(Waypoint(-2, 4, 3, 1, 15))
+    waypoints.append(Waypoint(-5, 8, 1, 5, 20))
+    waypoints.append(Waypoint(7, -3, 2, 4, 28))
+
 
     # Total number of segments
     numSegments = len(waypoints) - 1
@@ -67,50 +70,48 @@ if __name__ == "__main__":
         # Equality matrix (left side)
         # =============================
         A = zeros((numConstraints, numCoefficients))
-        # "start of segment" position constraints
-        # X Position
+        # X Position Start
         A[0,0] = waypoints[i].time ** 4
         A[0,1] = waypoints[i].time ** 3
         A[0,2] = waypoints[i].time ** 2
         A[0,3] = waypoints[i].time
         A[0,4] = 1
-        # Y Position
+        # Y Position Start
         A[1, 5] = waypoints[i].time ** 4
         A[1, 6] = waypoints[i].time ** 3
         A[1, 7] = waypoints[i].time ** 2
         A[1, 8] = waypoints[i].time
         A[1, 9] = 1
-        # Z Position
+        # Z Position Start
         A[2, 10] = waypoints[i].time ** 4
         A[2, 11] = waypoints[i].time ** 3
         A[2, 12] = waypoints[i].time ** 2
         A[2, 13] = waypoints[i].time
         A[2, 14] = 1
-        # Phi Angle
+        # Phi Angle Start
         A[3, 15] = waypoints[i].time ** 2
         A[3, 16] = waypoints[i].time
         A[3, 17] = 1
 
-        # "end of segment" position constraints
-        # X Position
+        # X Position End
         A[4, 0] = waypoints[i + 1].time ** 4
         A[4, 1] = waypoints[i + 1].time ** 3
         A[4, 2] = waypoints[i + 1].time ** 2
         A[4, 3] = waypoints[i + 1].time
         A[4, 4] = 1
-        # Y Position
+        # Y Position End
         A[5, 5] = waypoints[i + 1].time ** 4
         A[5, 6] = waypoints[i + 1].time ** 3
         A[5, 7] = waypoints[i + 1].time ** 2
         A[5, 8] = waypoints[i + 1].time
         A[5, 9] = 1
-        # Z Position
+        # Z Position End
         A[6, 10] = waypoints[i + 1].time ** 4
         A[6, 11] = waypoints[i + 1].time ** 3
         A[6, 12] = waypoints[i + 1].time ** 2
         A[6, 13] = waypoints[i + 1].time
         A[6, 14] = 1
-        # Phi Angle
+        # Phi Angle End
         A[7, 15] = waypoints[i + 1].time ** 2
         A[7, 16] = waypoints[i + 1].time
         A[7, 17] = 1
@@ -147,24 +148,24 @@ if __name__ == "__main__":
         A[14, 11] = 6 * waypoints[i].time
         A[14, 12] = 2
         # Phi Acceleration Rendezvous
-        A[15, 15] = 2
+        #A[15, 15] = 2
 
         # X Jerk Rendezvous
-        A[16, 0] = 24 * waypoints[i].time
-        A[16, 1] = 6
+        #A[16, 0] = 24 * waypoints[i].time
+        #A[16, 1] = 6
         # Y Jerk Rendezvous
-        A[17, 5] = 24 * waypoints[i].time
-        A[17, 6] = 6
+        #A[17, 5] = 24 * waypoints[i].time
+        #A[17, 6] = 6
         # Z Jerk Rendezvous
-        A[18, 10] = 24 * waypoints[i].time
-        A[18, 11] = 6
+        #A[18, 10] = 24 * waypoints[i].time
+        #A[18, 11] = 6
 
         # X Snap Rendezvous
-        A[19, 0] = 24
+        #A[19, 0] = 24
         # Y Snap Rendezvous
-        A[20, 5] = 24
+        #A[20, 5] = 24
         # Z Snap Rendezvous
-        A[21, 10] = 24
+        #A[21, 10] = 24
 
         # =============================
         # Equality vector (right side)
@@ -182,8 +183,14 @@ if __name__ == "__main__":
 
         # Derivatives = 0 for absolute Start
         if i != 0:
-            print("TODO: Set b Constraints for Segment Start")
+            b[8, 0] = 4 * trajectory[-1][0] * waypoints[i].time ** 3 + 3 * trajectory[-1][1] * waypoints[i].time ** 2 + 2 * trajectory[-1][2] * waypoints[i].time + trajectory[-1][3]
+            b[9, 0] = 4 * trajectory[-1][5] * waypoints[i].time ** 3 + 3 * trajectory[-1][6] * waypoints[i].time ** 2 + 2* trajectory[-1][7] * waypoints[i].time + trajectory[-1][8]
+            b[10, 0] = 4 * trajectory[-1][10] * waypoints[i].time ** 3 + 3 * trajectory[-1][11] * waypoints[i].time ** 2 + 2 * trajectory[-1][12] * waypoints[i].time + trajectory[-1][13]
+            b[11, 0] = 2 * trajectory[-1][15] * waypoints[i].time + trajectory[-1][16]
 
+            b[12, 0] = 12 * trajectory[-1][0] * waypoints[i].time ** 2 + 6 * trajectory[-1][1] * waypoints[i].time + 2 * trajectory[-1][2]
+            b[13, 0] = 12 * trajectory[-1][5] * waypoints[i].time ** 2 + 6 * trajectory[-1][6] * waypoints[i].time + 2 * trajectory[-1][7]
+            b[14, 0] = 12 * trajectory[-1][10] * waypoints[i].time ** 2 + 6 * trajectory[-1][11] * waypoints[i].time + 2 * trajectory[-1][12]
 
         # A = csc_matrix(A)  # convert to CSC for performance
         b = hstack(b)  # convert to hstack for performance
@@ -193,12 +200,16 @@ if __name__ == "__main__":
         qp_A = csc_matrix(qp_A)
         qp_l = hstack([l, b])
         qp_u = hstack([h, b])
+
+        # Setup solver and solve
         m = osqp.OSQP()
         m.setup(P=P, q=q, A=qp_A, l=qp_l, u=qp_u)
         res = m.solve()
+
+        # Save to trajectory variable
         trajectory.append(res.x)
         print("QP solution Number ", i, "following: ", res.x)
 
+    draw.draw_traj(waypoints, trajectory)
     # trajectory = solve_qp(P, q, G, h, A, b, solver="osqp")  # solver = "quadprog" (default), "cvxpy", "osqp"
-    # draw.draw_traj(waypoint0, waypoint1, trajectory)
     # return waypoint0, waypoint1, trajectory
